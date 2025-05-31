b5529de81ff892ef41741aac64726b7111216e97
Borna <80528615+borna-misetic@users.noreply.github.com>
2025-05-31 17:29:13 +0200

    hm

--- spawner_component.gd
+++ spawner_component.gd
@@ -1,16 +1,25 @@
 extends Node
 
-@export var currencyTimer := 0.3
-@export var spawnableLUT : Array[PackedScene]
+@export var currencyTimer := 0.2
+@export var enemyTimer := 1.4
+@export var spawnableCurrency : PackedScene
+@export var spawnableEnemy : PackedScene
 @export var player : CharacterBody2D
 @export var world : Node2D
 
 
 func _process(delta: float) -> void:
 	currencyTimer -= delta
-	if(player.velocity != Vector2.ZERO && currencyTimer < 0):
-		currencyTimer = 0.3
-		var randSpawnable = spawnableLUT[randi() % spawnableLUT.size()]
-		var randSpawnInstance = randSpawnable.instantiate()
+	enemyTimer -= delta
+	if(player.velocity != Vector2.ZERO):
+		if(currencyTimer < 0):
+			currencyTimer = 0.2
+			instantiateObject(spawnableCurrency)
+		if(enemyTimer < 0):
+			enemyTimer = 1.4
+			instantiateObject(spawnableEnemy)
+			
+func instantiateObject(object: PackedScene) -> void:
+		var randSpawnInstance = object.instantiate()
 		randSpawnInstance.global_position = player.position + Vector2(ProjectSettings.get_setting("display/window/size/viewport_width"), 0).rotated(randf_range(0, 2*PI))
 		world.add_child(randSpawnInstance)
