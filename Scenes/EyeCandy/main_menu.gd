6474619d8405c0a51e71f260d448a202924140c6
Borna <80528615+borna-misetic@users.noreply.github.com>
2025-06-01 00:25:18 +0200

    asdfer

---
+++ Scenes/EyeCandy/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var mainPanel = $MainPanel
+@onready var settingsPanel = $SettingsPanel
+@onready var sfxSlider = $SettingsPanel/HBoxContainer/VBoxContainer/HBoxContainer/SFXSlider
+@onready var musicSlider = $SettingsPanel/HBoxContainer/VBoxContainer/HBoxContainer2/MusicSlider
+
+func _on_settings_button_pressed() -> void:
+	mainPanel.hide()
+	settingsPanel.show()
+
+
+func _on_back_button_pressed() -> void:
+	settingsPanel.hide()
+	mainPanel.show()
+
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(1, linear_to_db(sfxSlider.value))
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(2, linear_to_db(musicSlider.value))
---
+++ Scenes/audio_manager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+func playMainMenu():
+	$MainMenu.play()
+	
+
+func playStage1():
+	$Stage1.play()
+	
+
+func playStage2():
+	$Stage2.play()
+	
+func playStage3():
+	$Stage3.play()
+	
+func playClick():
+	$ClickElement.play()
+	
+func playHover():
+	$HoverElement.play()
+	
