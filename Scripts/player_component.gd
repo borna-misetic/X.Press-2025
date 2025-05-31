7fea3698e73a522503ecafdca3537268b2042a59
Janpu123 <jan.puzak@gmail.com>
2025-05-31 18:04:11 +0200

    Scale

--- Scripts/player_component.gd
+++ Scripts/player_component.gd
@@ -3,18 +3,17 @@ extends Node
 @export var camera : Camera2D
 @export var player : CharacterBody2D
 @export var speed := 500
-@export var animationPlayer : AnimationPlayer
+var time : float
 
 func _ready() -> void:
 	PlayerGlobal.register_player(player);
 	
 func _physics_process(delta: float) -> void:
+	time += delta
 	player.look_at(camera.get_global_mouse_position())
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var inertiaVelocity = Vector2(move_toward(player.velocity.x, direction.x * speed, delta * 750),move_toward(player.velocity.y, direction.y * speed, delta * 750))
 	player.velocity = inertiaVelocity
+	$"../PlayerSprite".scale.x = 1+sin(time*3)*0.05
+	$"../PlayerSprite".scale.y = 1+sin(time*3)*0.05
 	player.move_and_slide()
-	if(direction != Vector2.ZERO):
-		animationPlayer.play("move")
-	else:
-		animationPlayer.play("idle")
