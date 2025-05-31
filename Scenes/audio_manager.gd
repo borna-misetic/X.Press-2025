b65e8bd730e5d56d438b1c472945c925ca5df30b
Borna <80528615+borna-misetic@users.noreply.github.com>
2025-06-01 00:59:22 +0200

    Git me some help

--- Scenes/audio_manager.gd
+++ Scenes/audio_manager.gd
@@ -1,22 +1,5 @@
 extends Node
 
-func playMainMenu():
-	$MainMenu.play()
-	
-
-func playStage1():
-	$Stage1.play()
-	
-
-func playStage2():
-	$Stage2.play()
-	
-func playStage3():
-	$Stage3.play()
-	
-func playClick():
-	$ClickElement.play()
-	
-func playHover():
-	$HoverElement.play()
-	
+@onready var mainMenu = $MainMenu
+@onready var hoverElement = $HoverElement
+@onready var clickElement = $ClickElement
