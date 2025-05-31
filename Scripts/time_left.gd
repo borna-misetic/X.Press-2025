6309e32a2837a6115d0839637b35b55349fa28c3
Janpu123 <jan.puzak@gmail.com>
2025-06-01 01:25:51 +0200

    yar

---
+++ Scripts/time_left.gd
@@ -0,0 +1,21 @@
+extends Label
+
+@onready var done := false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position.x = 1679.0
+	modulate = Color.WHITE
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (done == false):
+		text = str(int(round($GODISCOMING.time_left)))
+		modulate = Color.RED
+	else: pass
+
+
+func _on_godiscoming_timeout() -> void:
+	done = true
+	position.x=position.x-95
+	text = "NOW!!"
