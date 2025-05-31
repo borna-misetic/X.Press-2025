e5396adba90edd32c71a42cce07b6bde6a6ac56f
Borna <80528615+borna-misetic@users.noreply.github.com>
2025-05-31 20:04:16 +0200

    asder

--- Scripts/upgrade_component.gd
+++ Scripts/upgrade_component.gd
@@ -4,6 +4,7 @@ var upgradeShown = false
 
 @onready var experienceBar = $VBoxContainer/XPBarContainer/XPBar
 @onready var levelIndicator = $VBoxContainer/XPBarContainer/LevelIndicator
+@onready var animationPlayer = $AnimationPlayer
 
 func updateExperienceBar(currentXP, nextLevelXP, level):
 	experienceBar.max_value = nextLevelXP
@@ -11,4 +12,6 @@ func updateExperienceBar(currentXP, nextLevelXP, level):
 	levelIndicator.text = "LEVEL: " +  str(level)
 
 func showUpgrade():
-	
+	if(!upgradeShown):
+		animationPlayer.play("slide_in")
+		upgradeShown = true
