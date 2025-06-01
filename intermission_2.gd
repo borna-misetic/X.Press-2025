ec5b50932850a6ee8d068d782e68081c3f9ae6ef
Borna <80528615+borna-misetic@users.noreply.github.com>
2025-06-01 07:39:16 +0200

    asdrrr

--- intermission_2.gd
+++ intermission_2.gd
@@ -6,4 +6,6 @@ func _ready() -> void:
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "scene":
-		get_tree().change_scene_to_file("res://Scenes/main2.tscn")
+		AudioManager.stage2.stop()
+		AudioManager.stage3.play()
+		get_tree().change_scene_to_file("res://Scenes/city.tscn")
